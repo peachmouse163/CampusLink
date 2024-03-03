c47fd3bb1054b2c72983bcaf249cfdb4048479a4
peachmouse163 <lwx11574758@163.com>
2024-03-03 19:39:52 +0800

    +login +thin.xml

---
+++ app/src/main/java/com/example/campuslink/thin/ThinActivity.java
@@ -0,0 +1,16 @@
+package com.example.campuslink.thin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.campuslink.R;
+
+public class ThinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thin);
+    }
+}
